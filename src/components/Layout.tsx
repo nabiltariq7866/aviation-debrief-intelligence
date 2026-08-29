6534107d089b1552bcc0fdff29142ec0deddd221
nabiltariq7866 <nabiltariq7866@gmail.com>
2026-08-29 14:45:09 +0500

    Prevent theme flash on page load

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -59,6 +59,7 @@ export default function Layout(){
   useEffect(()=>{
     document.documentElement.classList.remove('dark','light')
     document.documentElement.classList.add(theme)
+    document.documentElement.style.colorScheme=theme
     localStorage.setItem('aviation-theme',theme)
   },[theme])
 
